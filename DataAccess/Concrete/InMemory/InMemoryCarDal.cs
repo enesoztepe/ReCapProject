be8d3cd4a832ef38a3b36ef80c772ba917d46d60
Enes Öztepe <enesoztepebjk@gmail.com>
2021-02-05 21:51:55 +0300

    DataAccess Katmanı tamamlandı.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{ Id = 1 , BrandId = 1 , ModelYear = 2000 , DailyPrice = 25 , Description = "Bu 2000 model bir arabadır" },
+                new Car{ Id = 2 , BrandId = 2 , ModelYear = 1995 , DailyPrice = 20 , Description = "Bu 1995 model bir arabadır"},
+                new Car{ Id = 3 , BrandId = 2 , ModelYear = 2005 , DailyPrice = 50 , Description = "Bu 2005 model bir arabadır"},
+                new Car{ Id = 4 , BrandId = 3 , ModelYear = 2015 , DailyPrice = 130 , Description = "Bu 2015 model bir arabadır"},
+                new Car{ Id = 5 , BrandId = 2 , ModelYear = 2020 , DailyPrice = 700 , Description = "Bu 2020 model bir arabadır"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int Id)
+        { // Id ler Pk olduğundan tek bir kayıt gelcektir.
+            return (Car)_cars.Where(c => c.Id == Id);
+        }
+
+
+    }
+}
--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -9,7 +9,7 @@ namespace Entities.Concrete
     {
         public int Id { get; set; }
         public int BrandId { get; set; }
-        public string ModelYear { get; set; }
+        public int ModelYear { get; set; }
         public double DailyPrice { get; set; }
         public string Description { get; set; }
     }
