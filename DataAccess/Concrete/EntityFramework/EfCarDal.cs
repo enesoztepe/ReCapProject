b57d53cc41c06391d59e7f4b8e92da767c339c40
Enes Öztepe <enesoztepebjk@gmail.com>
2021-02-08 14:02:27 +0300

    Core katmanı eklendi ve gerekli nesneler taşınıp sistem adapte edildi (1,2,3)

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,51 +10,8 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal : ICarDal
+    public class EfCarDal : EfEntityRepositoryBase<Car,TransportationContext>,ICarDal
     {
-        public void Add(Car entity)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
 
-        public void Delete(Car entity)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-        public void Update(Car entity)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                return filter == null
-                    ? context.Set<Car>().ToList()
-                    : context.Set<Car>().Where(filter).ToList();
-            }
-        }
-        public Car Get(Expression<Func<Car, bool>> filter)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                return context.Set<Car>().SingleOrDefault(filter);
-            }
-        }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,51 +10,8 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColorDal : IColorDal
+    public class EfColorDal : EfEntityRepositoryBase<Color, TransportationContext>, IColorDal
     {
-        public void Add(Color entity)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
 
-        public void Delete(Color entity)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-        public void Update(Color entity)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                return filter == null
-                    ? context.Set<Color>().ToList()
-                    : context.Set<Color>().Where(filter).ToList();
-            }
-        }
-        public Color Get(Expression<Func<Color, bool>> filter)
-        {
-            using (TransportationContext context = new TransportationContext())
-            {
-                return context.Set<Color>().SingleOrDefault(filter);
-            }
-        }
     }
 }
