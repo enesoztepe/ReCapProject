4b7ba166aa2da7722658c82764d784ea51b82054
enesoztepe <enesoztepebjk@gmail.com>
2021-03-05 15:19:39 +0300

    JWT geliştirme commit 2

---
+++ Core/Untilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Untilities.Security.JWT
+{
+    public class AccessToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
